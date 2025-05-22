9bcb6f6835547d7549269ca24c09745c4b219d20
Pedro_Stein <zodtewill@gmail.com>
2025-05-22 12:54:55 -0300

    feat: update gradeService com getSemesters e corrigir getCourses

--- src/services/gradeService.ts
+++ src/services/gradeService.ts
@@ -1,5 +1,5 @@
 import raw from "~/data/data.json";
-import type { GradeData, Shift, Course } from "~/types";
+import type { GradeData, Shift, Course, Semester, Day } from "~/types";
 
 const gradeData: GradeData = raw as GradeData;
 
@@ -10,4 +10,23 @@ export function getShifts(): Shift[] {
 export function getCourses(shiftName: string): Course[] {
   const shift = gradeData.shifts.find((s: Shift) => s.name === shiftName);
   return shift?.courses ?? [];
+}
+
+export function getSemesters(
+  shiftName: string,
+  coursesName: string,
+): Semester[] {
+  const shift = gradeData.shifts.find((s) => s.name === shiftName);
+  const course = shift?.courses.find((c) => c.name === coursesName);
+  return course?.semesters ?? [];
+}
+
+export function getDays(
+  shiftName: string,
+  courseName: string,
+  semesterNumber: number
+): Day[] {
+  const semesters = getSemesters(shiftName, courseName);
+  const semester = semesters.find((sem) => sem.number === semesterNumber);
+  return semester?.days ?? [];
 }
