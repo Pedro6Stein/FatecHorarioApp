817cb301fd3987f69e14cf6360925ba78f7dcc02
Pedro_Stein <zodtewill@gmail.com>
2025-05-22 17:27:38 -0300

    feat: adicionar ScheduleScreen e registrá-la no AppNavigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CourseListScreen from "~/screens/CourseListScreen";
 import HomeScreen from "~/screens/HomeScreen";
+import ScheduleScreen from "~/screens/ScheduleScreen";
  
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function AppNavigator() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="CourseList" component={CourseListScreen} />
+         <Stack.Screen name="Schedule"  component={ScheduleScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/ScheduleScreen.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, FlatList, TouchableOpacity, Text } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+
+import { getSemesters } from "~/services/gradeService";
+import type { Semester } from "~/types";
+
+import { HeaderAnimated } from "~/components/HeaderAnimated";
+import { OptionList } from "~/components/OptionList";
+
+
+export default function SchedulesScreeen() {
+
+    const { shiftName, courseName } = useRoute().params as {
+        shiftName: string;
+        courseName: string;
+    };
+
+    const nav = useNavigation<any>();
+    const sems: Semester[] = getSemesters(shiftName, courseName);
+
+    const options = sems.map((sem) => ({
+        key: String(sem.number),
+        label: `Semestre ${sem.number}`,
+        onPress: () =>
+            nav.navigate("DayList", {
+                shiftName,
+                courseName,
+                semesterNumber: sem.number,
+            }),
+    }));
+
+    return (
+    <SafeAreaView className="flex-1 bg-blue-800">
+      <HeaderAnimated
+        title={courseName}
+        subtitle="Selecione o semestre"
+      />
+      <OptionList options={options} />
+    </SafeAreaView>
+  );
+  
+};
